e73267d1e75151925c09f4b888195a2fdd0d5f7d
Radhika Choubey <radhikachoubey5@gmail.com>
2026-09-14 16:03:01 +0530

    Remove Netlify redirect

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import AllServices from "./pages/AllServices";
 import AllPackages from "./pages/AllPackages";
-import gallery from "./pages/gallery";
+import Gallery from "./pages/gallery";
 // import Contact from "./pages/Contact";
 import Inquiry from "./pages/Inquiry";
 
